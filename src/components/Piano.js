459f716eab8518b42219be7fd75d81cb4cf897cc
segunsub <segunsubair3@gmail.com>
2021-07-21 16:44:37 -0400

    piano css

--- src/components/Piano.js
+++ src/components/Piano.js
@@ -1,33 +1,33 @@
-import sound from '../samples/pianoSample'
+import piano from '../samples/pianoSample'
 import Buttons from './Buttons';
 
 function Piano() {
     return (
-        <div>
-            <Buttons sound={sound.C3} note='C3'/>
-            <Buttons sound={sound.Cs3} note='C#3'/>
-            <Buttons sound={sound.D3} note='D3'/>
-            <Buttons sound={sound.Ds3} note='D#3'/>
-            <Buttons sound={sound.E3} note='E3'/>
-            <Buttons sound={sound.F3} note='F3'/>
-            <Buttons sound={sound.Fs3} note='F#3'/>
-            <Buttons sound={sound.G3} note='G3'/>
-            <Buttons sound={sound.Gs3} note='G#3'/>
-            <Buttons sound={sound.A3} note='A3'/>
-            <Buttons sound={sound.As3} note='A#3'/>
-            <Buttons sound={sound.B3} note='B3'/>
-            <Buttons sound={sound.C4} note='C4'/>
-            <Buttons sound={sound.Cs4} note='C#4'/>
-            <Buttons sound={sound.D4} note='D4'/>
-            <Buttons sound={sound.Ds4} note='D#4'/>
-            <Buttons sound={sound.E4} note='E4'/>
-            <Buttons sound={sound.F4} note='F4'/>
-            <Buttons sound={sound.Fs4} note='F#4'/>
-            <Buttons sound={sound.G4} note='G4'/>
-            <Buttons sound={sound.Gs4} note='G#4'/>
-            <Buttons sound={sound.A4} note='A4'/>
-            <Buttons sound={sound.As4} note='A#4'/>
-            <Buttons sound={sound.B4} note='B4'/>
+        <div className="board">
+            <Buttons piano={piano.C3} buttonClass="flat" note='C3'/>
+            <Buttons piano={piano.Cs3} buttonClass="sharp" note='C#3'/>
+            <Buttons piano={piano.D3} buttonClass="flat" note='D3'/>
+            <Buttons piano={piano.Ds3} buttonClass="sharp" note='D#3'/>
+            <Buttons piano={piano.E3} buttonClass="flat" note='E3'/>
+            <Buttons piano={piano.F3} buttonClass="flat" note='F3'/>
+            <Buttons piano={piano.Fs3} buttonClass="sharp" note='F#3'/>
+            <Buttons piano={piano.G3} buttonClass="flat" note='G3'/>
+            <Buttons piano={piano.Gs3} buttonClass="sharp" note='G#3'/>
+            <Buttons piano={piano.A3} buttonClass="flat" note='A3'/>
+            <Buttons piano={piano.As3} buttonClass="sharp" note='A#3'/>
+            <Buttons piano={piano.B3} buttonClass="flat" note='B3'/>
+            <Buttons piano={piano.C4} buttonClass="flat" note='C4'/>
+            <Buttons piano={piano.Cs4} buttonClass="sharp" note='C#4'/>
+            <Buttons piano={piano.D4} buttonClass="flat" note='D4'/>
+            <Buttons piano={piano.Ds4} buttonClass="sharp" note='D#4'/>
+            <Buttons piano={piano.E4} buttonClass="flat" note='E4'/>
+            <Buttons piano={piano.F4} buttonClass="flat" note='F4'/>
+            <Buttons piano={piano.Fs4} buttonClass="sharp" note='F#4'/>
+            <Buttons piano={piano.G4} buttonClass="flat" note='G4'/>
+            <Buttons piano={piano.Gs4} buttonClass="sharp" note='G#4'/>
+            <Buttons piano={piano.A4} buttonClass="flat" note='A4'/>
+            <Buttons piano={piano.As4} buttonClass="sharp" note='A#4'/>
+            <Buttons piano={piano.B4} buttonClass="flat" note='B4'/>
         </div>
     )
 }
